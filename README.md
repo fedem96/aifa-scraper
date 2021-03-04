fd142d55aff71c6de6ff27c56b48abff38348912
Federico Magnolfi <federicomagno96@gmail.com>
2021-03-04 13:32:52 +0100

    added README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# AIFA scraper
+
+(in progress)
+
+## Usage
+
+### Compile
+
+Linux/macOS: `./gradlew war`
+
+Windows: `gradlew.bat war` 
+
+### Docker
+`docker-compose up`
+
+### Endpoint
+`{url}/aifa-scraper/api`
--- src/main/java/com/fedem96/rest/APIApplication.java
+++ src/main/java/com/fedem96/rest/APIApplication.java
@@ -1,7 +1,21 @@
 package com.fedem96.rest;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @ApplicationPath("api")
-public class APIApplication extends Application {}
+@Path("/")
+public class APIApplication extends Application {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response hello(){
+        return Response.ok("Hello from AIFA-scraper server").build();
+    }
+
+}
